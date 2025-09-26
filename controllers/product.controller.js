4f783f28e779ac0afbbce272335d71edd24619c4
4rthemis <syahrul.uix@gmail.com>
2025-09-26 11:08:48 +0700

    update (catalogProduct) : change product catalog feature

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,18 +1,29 @@
-const products = require('../data/productsData.json'); 
-
+const products = require('../data/productsData.json');
+const users = require('../data/user.json');
 
 exports.createProduct = (req, res) => {
   const { productName, productCategory, price, owner } = req.body;
 
-  // cek kalau nama produk sudah ada
+  // cek kalau nama produk sudah ada dan harus unik
   if (products.find(p => p.productName === productName)) {
     return res.status(400).json({ success: false, message: "Product name must be unique!" });
   }
 
+  // cek user berdasarkan owner
+  const user = users.find(u => u.username === owner);
+  if (!user) {
+    return res.status(404).json({ success: false, message: "User not found!" });
+  }
+
+  // cek role user
+  if (user.role !== "seller") {
+    return res.status(403).json({ success: false, message: "Only sellers can add products!" });
+  }
+
+  // Tambahkan produk
   const newProduct = { productName, productCategory, price, owner };
   products.push(newProduct);
-  res.status(201).json(newProduct);
-  
+  return res.status(201).json({ success: true, message: "Product added successfully!", product: newProduct });
 };
 
 exports.getProducts = (res) => {
@@ -21,6 +32,8 @@ exports.getProducts = (res) => {
 
 exports.getProductByName = (req, res) => {
   const product = products.find(p => p.productName === req.params.product_name);
-  if (!product) return res.status(404).json({ success: false, message: "Product not found :(" });
+  if (!product) {
+    return res.status(404).json({ success: false, message: "Product not found :(" });
+  }
   res.json(product);
 };
