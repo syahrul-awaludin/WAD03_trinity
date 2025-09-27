d5c378942b21d58126cf893f541f8390ad06c1b2
Sisil <Sisiliafransisca25@gmail.com>
2025-09-27 10:12:15 +0700

    update serve.js sementara

--- serve.js
+++ serve.js
@@ -1,16 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const aboutUsRoutes = require("./routes/aboutUsRoutes");
-const userRoutes = require("./routes/users"); // import user routes
-const app = express();
+const userRoutes = require("./routes/users");
+const cartRoutes = require("./routes/cart");
 
+const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
+
 app.use("/", aboutUsRoutes);
 app.use("/", userRoutes);
+app.use("/", cartRoutes);
+
 
 app.use((err, req, res, next) => {
 	console.error(err.stack);
@@ -30,4 +36,11 @@ app.listen(PORT, () => {
 	console.log(`- GET /api/greeting?name=YourName`);
 	console.log(`- GET /api/greeting/:name`);
 	console.log(`- POST /api/greeting`);
+	console.log(`- POST /users`);
+	console.log(`- GET /users`);
+	console.log(`- GET /users/:username`);
+	console.log(`- PATCH /users/:username`);
+	console.log(`- POST /carts/:username/add`);
+	console.log(`- POST /carts/:username/remove`);
+	console.log(`- GET /carts/:username`);
 });
